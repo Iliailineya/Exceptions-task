6948bea03b082755543b71e43e1a356ad6b7b68b
Iliailineya <ilavituk011@gmail.com>
2023-08-10 16:07:59 +0300

    Homework 1 task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            String input = scanner.nextLine();
+            int number = Integer.parseInt(input);
+
+            System.out.println("The number is: " + number);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: value is not a correct number.");
+        }
     }
 }
